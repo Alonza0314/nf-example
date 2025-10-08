46ab3e8b027a4ea8fc03131b678f10a34dfaf9e2
name <83965216+Jasonkuuo@users.noreply.github.com>
2025-10-08 13:42:25 +0800

    fix some bugs

--- internal/sbi/api_attendence.go
+++ internal/sbi/api_attendence.go
@@ -44,5 +44,9 @@ func (s *Server) PostAttendence(c *gin.Context) {
 	}
 
 	targetName := string(body)
+	if targetName == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "no name provided"})
+		return
+	}
 	s.Processor().PostAttendence(c, targetName)
 }
