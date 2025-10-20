bdd7d9053350cd2e1e9a2fbf7282914209374004
wiwi <a31625802@gmail.com>
2025-10-20 06:33:54 +0000

    chore: format api_default.go to satisfy gci

--- internal/sbi/api_default.go
+++ internal/sbi/api_default.go
@@ -18,19 +18,19 @@ func (s *Server) getDefaultRoute() []Route {
 			// Use
 			// curl -X GET http://127.0.0.163:8000/default/ -w "\n"
 		},
-		{ 	
-			Name: "Exercise GET",
-			Method: http.MethodGet,
+		{
+			Name:    "Exercise GET",
+			Method:  http.MethodGet,
 			Pattern: "/exercise",
 			APIFunc: func(c *gin.Context) {
 				c.JSON(http.StatusOK, "This is get")
 			},
 		},
-		{ 	
-			Name: "Exercise POST",
-			Method: http.MethodPost,
+		{
+			Name:    "Exercise POST",
+			Method:  http.MethodPost,
 			Pattern: "/exercise",
-			APIFunc: func(c *gin.Context) { 
+			APIFunc: func(c *gin.Context) {
 				c.JSON(http.StatusOK, "This is post")
 			},
 		},
